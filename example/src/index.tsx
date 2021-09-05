a093093210b355b612f865f2156dc2dd98e43e7a
Turtlemay <am@trtl.in>
2021-09-05 13:47:14 -0400

    Added function component example.

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,22 +2,18 @@ import * as React from '../node_modules/@turtlemay/jsx-dom/out/index.js'
 
 import Component from './Component.js'
 
-@Component.register()
-class MyCustomElement extends Component {
-  private _pElem: HTMLParagraphElement
-
-  public connectedCallback() {
-    this._render(
-      <p ref={v => this._pElem = v}>
-        hello {MyCustomElement.name}
-      </p>
-    )
-    console.assert(this.className === 'test')
-    console.assert(this.style.display === 'block')
-    console.assert(this.getAttribute('test-string') === 'foo')
-    console.assert(this.getAttribute('test-boolean') === null)
-    console.assert(this.getAttribute('test-number') === '0')
+function MyFunctionComponent() {
+  console.log(`hello ${MyFunctionComponent.name}`)
+  
+  function onClick(e: Event) {
+    console.log(e.type, e.currentTarget)
   }
+  
+  return (
+    <div {...{onClick}}>
+      <p>hello {MyFunctionComponent.name}</p>
+    </div>
+  )
 }
 
 const testAttribs = {
@@ -37,10 +33,32 @@ const testAttribs = {
   'test-object': { foo: 'foo', },
 }
 
+@Component.register()
+class MyCustomElement extends Component {
+  private _pElem: HTMLParagraphElement
+
+  public connectedCallback() {
+    this._render(
+      <p ref={v => this._pElem = v}>
+        hello {MyCustomElement.name}
+      </p>
+    )
+    console.assert(this.className === 'test')
+    console.assert(this.style.display === 'block')
+    console.assert(this.getAttribute('test-string') === 'foo')
+    console.assert(this.getAttribute('test-boolean') === null)
+    console.assert(this.getAttribute('test-number') === '0')
+  }
+}
+
 document.body.appendChild(
   <div {...testAttribs}>
     <p children="hello div" />
   </div>
+)
+
+document.body.appendChild(
+  <MyFunctionComponent />
 )
 
 document.body.appendChild(
